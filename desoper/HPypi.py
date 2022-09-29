1a706ea8d8f71dab92c9586ae329ad9eaa3a1bcf
Sagamez24 <111073837+Sagamez24@users.noreply.github.com>
2022-09-29 16:53:49 -0500

    Update HPypi.py

--- desoper/HPypi.py
+++ desoper/HPypi.py
@@ -25,20 +25,7 @@ def valor_m(n):
     return m
 
 
-def _get_chiral(q,q_max=np.inf):
-    #Normalize to positive minimum
-    if 0 in q:
-        return None,None  #Quita los ceros
-    if q[0]<0:
-        q=-q
-    #Divide by GCD
-    GCD=np.gcd.reduce(q)
-    q=(q/GCD).astype(int)
-    if ( 0 not in [ sum(p) for p in itertools.permutations(q, 2) ] and np.abs(q).max()<=q_max):
-        return q,GCD #not 0 in q, #avoid vector-like and multiple 0's ##De todos los elementos 
-                      #de q, tome el más grande y positivo y menor que infinito
-    else:
-        return None,None
+
     
 #if __name__ == '__main__':
 #    r'''
