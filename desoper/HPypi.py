7b2cf70c02673157f428e5c38e5c6d002f316b64
Sagamez24 <111073837+Sagamez24@users.noreply.github.com>
2022-09-29 17:15:10 -0500

    Update HPypi.py

--- desoper/HPypi.py
+++ desoper/HPypi.py
@@ -2,8 +2,8 @@
 r'''
 Hello moduleHPypi module
 '''
-import dask.array as da
-#import numpy as np
+#import dask.array as da
+import numpy as np
 from functools import cached_property
 #import random
 #import itertools
@@ -25,30 +25,20 @@ def valor_m(n):
         m = (n-3)/2
     return m
 
-def vector_like(n, M, Nmax):
-    """
-    Esta funcion devuelve los vetoreslikes v_mas y v_menos o u_mas y u_menos
-    a partir de los arreglos L y K tomados de la lista aleatoria con valores
-    entre -15 y 15
-    """
-    m = int(valor_m(n))
-   
-#    print(lista)
-     
-    if n%2 == 0:
-        
-        lista = da.random.randint(-M, M+1, (Nmax, 2*m)) 
-        lista = lista.to_dask_dataframe().drop_duplicates().to_dask_array()
-       
-        Z_sol=lista
-                
+def _get_chiral(q,q_max=np.inf):
+    #Normalize to positive minimum
+    if 0 in q:
+        return None,None  #Quita los ceros
+    if q[0]<0:
+        q=-q
+    #Divide by GCD
+    GCD=np.gcd.reduce(q)
+    q=(q/GCD).astype(int)
+    if ( 0 not in [ sum(p) for p in itertools.permutations(q, 2) ] and np.abs(q).max()<=q_max):
+        return q,GCD #not 0 in q, #avoid vector-like and multiple 0's ##De todos los elementos 
+                      #de q, tome el más grande y positivo y menor que infinito
     else:
-        lista = da.random.randint(-M, M+1, (Nmax, 2*m+1)) 
-        lista = lista.to_dask_dataframe().drop_duplicates().to_dask_array()
-      
-
-        Z_sol=lista              
-    return Z_sol
+        return None,None
 
     
 #if __name__ == '__main__':
