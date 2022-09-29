561512845ee752f2472f3476897bb0f400cb6bc8
Sagamez24 <111073837+Sagamez24@users.noreply.github.com>
2022-09-29 16:15:09 -0500

    Update HPypi.py

--- desoper/HPypi.py
+++ desoper/HPypi.py
@@ -20,30 +20,6 @@ def valor_m(n):
         m = (n-3)/2
     return m
 
-def vector_like(n, M, Nmax):
-    """
-    Esta funcion devuelve los vetoreslikes v_mas y v_menos o u_mas y u_menos
-    a partir de los arreglos L y K tomados de la lista aleatoria con valores
-    entre -15 y 15
-    """
-    m = int(valor_m(n))
-   
-#    print(lista)
-     
-    if n%2 == 0:
-        
-        lista = da.random.randint(-M, M+1, (Nmax, 2*m)) 
-        lista = lista.to_dask_dataframe().drop_duplicates().to_dask_array()
-       
-        Z_sol=lista
-                
-    else:
-        lista = da.random.randint(-M, M+1, (Nmax, 2*m+1)) 
-        lista = lista.to_dask_dataframe().drop_duplicates().to_dask_array()
-      
-
-        Z_sol=lista              
-    return Z_sol
 #if __name__ == '__main__':
 #    r'''
 #    Hello main
