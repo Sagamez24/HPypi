e751a55f462395f99ecd9a3de3967d609b5c3d8e
Sagamez24 <111073837+Sagamez24@users.noreply.github.com>
2022-09-29 23:03:43 -0500

    Update HPypi.py

--- desoper/HPypi.py
+++ desoper/HPypi.py
@@ -6,6 +6,7 @@ import dask.array as da
 import numpy as np
 #from functools import cached_property
 #import random
+import pandas as pd
 from anomalies import anomaly
 import itertools
 from itertools import permutations
