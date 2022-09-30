3960b50b207c5ec1ac2db6ef2a7d79d1c74b4396
Sagamez24 <111073837+Sagamez24@users.noreply.github.com>
2022-09-29 21:58:25 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -69,8 +69,8 @@ def main():
         install_requires=['numpy==1.16.5; python_version=="3.7"',
             'numpy>=1.16.5; python_version=="3.8"',
             'numpy>=1.16.5; python_version=="3.9"',
-            #'dask==2022.2.1; python_version=="3.7"',
-            'dask==2022.2.1; python_version=="3.8"',
+            'dask==2022.2.1; python_version=="3.7"',
+            #'dask==2022.2.1; python_version=="3.8"',
             'dask==2022.2.1; python_version=="3.9"'],
     )
 
