3c6f2b1adabc13722d4eb584bf8d96d2978c5121
Sagamez24 <111073837+Sagamez24@users.noreply.github.com>
2022-09-30 15:14:07 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ if os.name in ('nt', 'dos'):
 def main():
     setup(
         # Application name:
-        name="desoper",
+        name="HPypi",
 
         # Version number (initial):
         version="0.0.3",
