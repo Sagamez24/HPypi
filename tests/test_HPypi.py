8402d5149cc8c6f6aa0330cf105ec8288421f397
Sagamez24 <111073837+Sagamez24@users.noreply.github.com>
2022-09-30 01:01:34 -0500

    Update test_HPypi.py

--- tests/test_HPypi.py
+++ tests/test_HPypi.py
@@ -10,13 +10,13 @@ from desoper.HPypi import solucion_total
  #                        'Hello, World!', True)
 
 class Test_solucion_total(unittest.TestCase):
-	   def test_5(self):
-	       sls_5 = solucion_total(5,6,10000),
-	       self.assertEqual(11, sls_5.shape[0], True) 
+      def test_5(self):
+	  sls_5 = solucion_total(5,6,10000),
+          self.assertEqual(11, sls_5.shape[0], True) 
    
-           def test_6(self):
-	       sls_6 = solucion_total(6,9,500000),
-	       self.assertEqual(141, sls_6.shape[0], True)
+      def test_6(self):
+	  sls_6 = solucion_total(6,9,500000),
+	  self.assertEqual(141, sls_6.shape[0], True)
             
 if __name__ == '__main__':
     unittest.main()
