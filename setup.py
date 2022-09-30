84c25f7671ee0cd64f4bda6f1557cf143b2ea97c
Sagamez24 <111073837+Sagamez24@users.noreply.github.com>
2022-09-30 15:34:01 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def main():
         name="HPypi",
 
         # Version number (initial):
-        version="0.0.4",
+        version="0.0.5",
 
         # Application author details:
         author="Hinestroza",
