09407b915c85fc6616335fb4819195904ca66c30
Sagamez24 <111073837+Sagamez24@users.noreply.github.com>
2022-09-30 00:54:41 -0500

    Update test_HPypi.py

--- tests/test_HPypi.py
+++ tests/test_HPypi.py
@@ -1,13 +1,22 @@
 import unittest
-from desoper import HPypi
+import pandas as pd
+from desoper.HPypi import solucion_total
 
 
-class Test_hello(unittest.TestCase):
-    def test__working(self):
-        self.assertEqual(1,1, True)
+#class Test_hello(unittest.TestCase):
+ #   def test__working(self):
+  #      self.assertEqual(1,1, True)
 #self.assertEqual(HPypi.hello(),
  #                        'Hello, World!', True)
 
-
+class Test_solucion_total(unittest.TestCase):
+	   def test_5(self):
+	      sls_5 = solucion_total(5,6,10000),
+	      self.assertEqual(11, sls_5.shape[0], True) 
+   
+       def test_6(self):
+	      sls_6 = solucion_total(6,9,500000),
+	      self.assertEqual(141, sls_6.shape[0], True)
+            
 if __name__ == '__main__':
     unittest.main()
