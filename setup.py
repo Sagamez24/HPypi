2b3c23a24e345b1e22ccdb277297426e484557ab
Sagamez24 <111073837+Sagamez24@users.noreply.github.com>
2022-09-29 21:59:47 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ def main():
             'numpy>=1.16.5; python_version=="3.8"',
             'numpy>=1.16.5; python_version=="3.9"',
             'dask==2022.2.1; python_version=="3.7"',
-            #'dask==2022.2.1; python_version=="3.8"',
+            'dask==2022.2.1; python_version=="3.8"',
             'dask==2022.2.1; python_version=="3.9"'],
     )
 
