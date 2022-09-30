df571457274b5d46712543be0c68041a9f70dfba
Sagamez24 <111073837+Sagamez24@users.noreply.github.com>
2022-09-30 01:03:26 -0500

    Update test_HPypi.py

--- tests/test_HPypi.py
+++ tests/test_HPypi.py
@@ -11,11 +11,11 @@ from desoper.HPypi import solucion_total
 
 class Test_solucion_total(unittest.TestCase):
       def test_5(self):
-	  sls_5 = solucion_total(5,6,10000),
+	  sls_5 = solucion_total(5,6,10000)
           self.assertEqual(11, sls_5.shape[0], True) 
    
       def test_6(self):
-	  sls_6 = solucion_total(6,9,500000),
+	  sls_6 = solucion_total(6,9,500000)
 	  self.assertEqual(141, sls_6.shape[0], True)
             
 if __name__ == '__main__':
